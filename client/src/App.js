8eb2aa078d7371878c69400ba9673c568a24c4db
Jyotirmoy Bhattacharjee <61322013+jyotirmoy99@users.noreply.github.com>
2022-03-08 21:27:32 +0530

    video stream - RTMP server

--- client/src/App.js
+++ client/src/App.js
@@ -17,15 +17,13 @@ function App() {
     <div className="ui container">
       <Router history={history}>
         <Header />
-        <div>
-          <Switch>
-            <Route path="/" exact component={StreamList} />
-            <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit/:id" exact component={StreamEdit} />
-            <Route path="/streams/delete/:id" exact component={StreamDelete} />
-            <Route path="/streams/show" exact component={StreamShow} />
-          </Switch>
-        </div>
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Route path="/streams/:id" exact component={StreamShow} />
+        </Switch>
       </Router>
     </div>
   );
